10a33b7d3f8ce03e5715f9c07a11e01a4b5f1273
Daddie0 <33762262+GoByeBye@users.noreply.github.com>
2021-08-09 00:44:58 +0200

    Add bridge finish from guilded

--- guildedbot.py
+++ guildedbot.py
@@ -7,6 +7,8 @@ import guilded
 from discord_webhook import DiscordWebhook
 import requests
 from dotenv import load_dotenv
+import re
+
 
 coloredlogs.install(level="INFO", fmt="%(levelname)s | %(asctime)s | %(name)s | %(message)s")
 
@@ -33,6 +35,32 @@ async def on_message(ctx):
         return # Ignore messages without an avatar
     cleanedeveryone = ctx.content.replace('@everyone', '@​everyone')
     cleanedhere = cleanedeveryone.replace('@here', '@​here')
+
+    
+    # Find all mentions in ctx.content
+    # Mention example <@d8Gb36p4> or <@adgt2342JKKF>
+    if '<@' in cleanedhere:
+        mentions = re.findall(r'<@([a-zA-Z0-9]+)>', cleanedhere)
+        with open("data/users.json", "r") as f:
+            users = json.load(f)
+            users = users["users"]
+
+        logging.info(mentions)
+        for mention in mentions:
+            # Remove stuff around mention so we only get the user id
+            mention = mention.replace('<@', '')
+            mention = mention.replace('>', '')
+            logging.info(mention)
+            # Check if mention is in the list of users
+            for user in users:
+                if user["guildedID"] == mention:
+                    logging.info("Mention matches linked account")
+                    oldMention = f"<@{mention}>"
+                    newMention = f"<@{user['discordID']}>"
+                    cleanedhere = cleanedhere.replace(oldMention, newMention)
+                    logging.info(cleanedhere)
+
+
     webhook = DiscordWebhook(url=os.getenv('DISCORD_WEBHOOK'), content=cleanedhere, username=ctx.author.name, avatar_url=ctx.author.avatar_url)
     response = webhook.execute()
 
@@ -64,6 +92,52 @@ async def on_message(ctx):
     else:
         logging.error("Message failed to send | Code: " + response.status_code)
 
+    
+
+    if ctx.content.startswith("bridge"):
+        # Split message into arguments (args are separated by spaces)
+        args = ctx.content.split(" ")
+        # Remove the prefix
+        args.pop(0)
+        
+        # if else spagetti for commands becuase guilded.py doesn't support cogs yet
+        logging.info(args)
+        if args[0] == "link":
+            if len(args) == 2:
+                logging.info("made it to link process")
+
+                # Check if the code exists in data/auth.json
+                with open("data/auth.json") as jsonFile:
+                    data = json.load(jsonFile)
+                    data = data["discord"]
+                    for id in data:
+                        if id["code"] == args[1]:
+                            logging.info("Matched code!")
+                            # If it does link the accounts
+                            logging.info("Linking accounts")
+                            guildedID = ctx.author.id
+                            # Grab the discord ID from the auth.json file in the same bit as the 
+                            discordID = id["discordID"]
+                            logging.info("Guilded ID: " + guildedID)
+                            logging.info("Discord ID: " + str(discordID))
+                            # Create a new entry in data/users.json with the discordID and guildedID
+                            # so that we can keep track of who is linked to who
+                            # Example: { "users": [ { "discordID": "123456789", "guildedID": "d8Gb36p4" } ] }
+
+                            with open("data/users.json", "r") as jsonFile:
+                                data = json.load(jsonFile)
+                                temp = data["users"]
+                                y = {"discordID" : int(discordID), "guildedID" : str(guildedID)}
+                                temp.append(y)
+                                write_json(data, "data/users.json")
+                
+                await ctx.channel.send("Sucessfully linked accounts")
+
+            else:
+                await ctx.channel.send("Start linking process from discord for now")
+                            
+
+            
 @bot.event()
 async def on_message_edit(ctx):
     logging.debug("Message edited: " + ctx.id)
